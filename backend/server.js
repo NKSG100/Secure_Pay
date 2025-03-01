b4f5ac9f427832fb917655c70e21dffdaefa46cf
NKSG100 <nksnamannks@gmail.com>
2025-03-02 00:30:06 +0530

    cors code added

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,13 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: [""],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 app.use(morgan("dev"));
 
 const authRoutes = require("./routes/authRoutes");
