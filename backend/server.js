6c0bcaa8c099be0dbecda6e6d45aa27334b26158
NKSG100 <nksnamannks@gmail.com>
2025-03-02 01:08:49 +0530

    cors error

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
   {
-    origin: [""],
+    origin: ["https://secure-pay-sepia.vercel.app/"],
     methods: ["POST", "GET"],
     credentials: true
   }
